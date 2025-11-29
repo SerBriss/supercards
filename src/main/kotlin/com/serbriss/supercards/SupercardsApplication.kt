090560b7235a83d3a708f0fe0bfc414c0494742c
Sergey Briss <sefimenko13@gmail.com>
2025-11-29 17:06:33 +0300

    init

---
+++ src/main/kotlin/com/serbriss/supercards/SupercardsApplication.kt
@@ -0,0 +1,11 @@
+package com.serbriss.supercards
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SupercardsApplication
+
+fun main(args: Array<String>) {
+    runApplication<SupercardsApplication>(*args)
+}
